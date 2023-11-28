488f05f12ed31dc2f82887466fae6b89e0b7c862
Tausif Raza <tausif00100@gmail.com>
2023-11-28 13:59:13 +0530

    Learned Pointers

--- CPP-Concepts/Pointer.cpp
+++ CPP-Concepts/Pointer.cpp
@@ -1,17 +1,26 @@
 /*
-Pointers: Adress of a value; When excutable file works then it loads in RAM.
+Pointers: Adress of a value; When excutable file works then it loads in RAM and it chagnes whenever you execute the programm;
+adress will be hexadecimal;
 
-int* a; = 42
-
+can we store the adress ;
+// we use pointers(variable) 
 
 */
 
 #include <iostream>
 using namespace std;
+int main() {
+    int x = 42;
+    //&: adress of operator: tells Ram adress of variables
+    cout <<  &x;
 
-int main()
-{
-int a = 42;
-cout<< a;
-
+    /* 
+    int y = 43;
+    int* ptrs = &y;
+    int* ptr = &x;  // ptr stores the memory address of x
+    using namespace std;
+    cout << "Value of x: " << x << endl;
+    cout << "Memory address of x: " << &x << endl;
+    cout << "Value at the memory address pointed to by ptr: " << *ptr <<endl;
+    cout << *ptrs <<endl; */
 }
